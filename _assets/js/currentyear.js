ed7af71f8c2873c08cc12812e9fd9d18735cf64b
Greg Raven <gregraven@mac.com>
2023-12-21 15:11:15 -0800

    initial re-load

---
+++ _assets/js/currentyear.js
@@ -0,0 +1,6 @@
+// returns the year (four digits)
+var currentYear = new Date().getFullYear();
+// create span element with
+// id of "currentyear", then
+// insert currentYear into document
+document.getElementById("currentyear").innerHTML = currentYear;
